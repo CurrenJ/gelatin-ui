5e41e2050f0fc77f9cd251ddf72430d11e638cba
Curren Jeandell <jeandell.curren@gmail.com>
2025-10-31 22:12:02 +0000

    Fix mixins by using separate mixin.json for each platform - Bump version to 1.0.10

--- common/src/main/java/io/github/currenj/gelatinui/mixin/HoverEventActionMixin.java
+++ common/src/main/java/io/github/currenj/gelatinui/mixin/HoverEventActionMixin.java
@@ -1,48 +1,29 @@
 package io.github.currenj.gelatinui.mixin;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.mojang.serialization.Codec;
-import io.github.currenj.gelatinui.GelatinUi;
-import io.github.currenj.gelatinui.tooltip.ItemStacksInfo;
 import io.github.currenj.gelatinui.tooltip.ItemStacksTooltip;
 import net.minecraft.network.chat.HoverEvent;
 import net.minecraft.util.StringRepresentable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.function.Supplier;
-
-@Mixin(HoverEvent.Action.class)
+@Mixin(value = HoverEvent.Action.class)
 public class HoverEventActionMixin {
-    @WrapOperation(
+    @Shadow public static Codec<HoverEvent.Action<?>> UNSAFE_CODEC;
+
+    @Inject(
             method = "<clinit>",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/util/StringRepresentable;fromValues(Ljava/util/function/Supplier;)Lcom/mojang/serialization/Codec;"
-            )
+            at = @At("TAIL")
     )
-    private static <T extends StringRepresentable> Codec<T> wrapFromValues(Supplier<T[]> supplier, Operation<Codec<T>> original) {
-        // Type should be HoverEvent.Action<?>. Fail gracefully if not.
-
-        if (!HoverEvent.Action.class.isAssignableFrom(supplier.get().getClass().getComponentType())) {
-            GelatinUi.LOGGER.error("Unexpected type in HoverEvent.Action.fromValues mixin: {}", supplier.get().getClass().getComponentType());
-            return original.call(supplier);
-        }
-
-        Supplier<HoverEvent.Action<?>[]> modifiedSupplier = () -> {
-            HoverEvent.Action<?>[] originalActions = (HoverEvent.Action<?>[]) supplier.get();
-            HoverEvent.Action<?>[] additionalActions = gelatinui$getAdditionalActions();
-            HoverEvent.Action<?>[] modifiedActions = new HoverEvent.Action<?>[originalActions.length + additionalActions.length];
-
-            System.arraycopy(originalActions, 0, modifiedActions, 0, originalActions.length);
-            System.arraycopy(additionalActions, 0, modifiedActions, originalActions.length, additionalActions.length);
-
-            return modifiedActions;
-        };
-
-        return original.call(modifiedSupplier);
+    private static void wrapFromValues(CallbackInfo ci) {
+        // Add alternative to codec, so when MC's codec fails to decode our custom action, fall back to our own codec
+        HoverEvent.Action.UNSAFE_CODEC = Codec.withAlternative(UNSAFE_CODEC,
+                StringRepresentable.fromValues(HoverEventActionMixin::gelatinui$getAdditionalActions));
+        HoverEvent.Action.CODEC = UNSAFE_CODEC.validate(HoverEvent.Action::filterForSerialization);
     }
 
     @Unique
